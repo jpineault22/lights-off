11ccd325aa943f4452d715cdbbe316deddcd0ac5
jpineault22 <julienjpineault@hotmail.com>
2023-03-06 14:06:27 -0500

    Add build 2.0 for Mac (.app and .dmg)

---
+++ LightsOff/Builds/2.0/Mac/Party's Over.app/Contents/Resources/Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,150 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID FADEINCREDITSAMBIANCE = 310753704U;
+        static const AkUniqueID FADEINFORLEVELTRANSITION = 3673038698U;
+        static const AkUniqueID FADEINFORPAUSEMENU = 4235771730U;
+        static const AkUniqueID FADEINTOMENU = 1810143960U;
+        static const AkUniqueID FADEOUTCREDITSAMBIANCE = 739742841U;
+        static const AkUniqueID FADEOUTFORCREDITS = 3904148240U;
+        static const AkUniqueID FADEOUTFORLEVELTRANSITION = 1730431133U;
+        static const AkUniqueID FADEOUTFORPAUSEMENU = 3632035365U;
+        static const AkUniqueID FADEOUTFORQUITTRANSITION = 63923110U;
+        static const AkUniqueID MUSIC_BACKTOMENU = 2575825050U;
+        static const AkUniqueID MUSIC_PLAYGAME = 2919733517U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+        static const AkUniqueID PLAY_BREAKEROFF = 2894801721U;
+        static const AkUniqueID PLAY_BREAKERON = 2293005965U;
+        static const AkUniqueID PLAY_CONVEYOR = 2492934017U;
+        static const AkUniqueID PLAY_CYCLESWITCH = 1153633900U;
+        static const AkUniqueID PLAY_DOORCLOSES = 3948844197U;
+        static const AkUniqueID PLAY_DOOROPENS = 1196441153U;
+        static const AkUniqueID PLAY_ENEMYLAND = 3004097409U;
+        static const AkUniqueID PLAY_ENEMYPASSOUT = 904139487U;
+        static const AkUniqueID PLAY_ENEMYSTARTCHASING = 2666922589U;
+        static const AkUniqueID PLAY_ENEMYSTOPCHASING = 1824093213U;
+        static const AkUniqueID PLAY_ENEMYSTUNNED = 871860279U;
+        static const AkUniqueID PLAY_ENEMYWAKEUP = 232698797U;
+        static const AkUniqueID PLAY_ENEMYWANDER = 1371404669U;
+        static const AkUniqueID PLAY_FAN = 2757069037U;
+        static const AkUniqueID PLAY_GATEMOVES = 3519553787U;
+        static const AkUniqueID PLAY_KEYCOLLECT = 3690359235U;
+        static const AkUniqueID PLAY_KEYGATEOPENS = 46699921U;
+        static const AkUniqueID PLAY_KEYLANDS = 4262882519U;
+        static const AkUniqueID PLAY_LADDERMOVES = 3073568824U;
+        static const AkUniqueID PLAY_MENUBUTTONBACKPRESS = 2694787811U;
+        static const AkUniqueID PLAY_MENUBUTTONPRESS = 1910628788U;
+        static const AkUniqueID PLAY_MENUCHANGESETTING = 1578733045U;
+        static const AkUniqueID PLAY_MENUDELETESAVEFILEBUTTONPRESS = 1030696368U;
+        static const AkUniqueID PLAY_MENUPLAY = 1022756005U;
+        static const AkUniqueID PLAY_MENUQUIT = 3159183902U;
+        static const AkUniqueID PLAY_MENUSELECTIONMOVE = 1654690896U;
+        static const AkUniqueID PLAY_MOVINGPLATFORMTYPEA = 904873966U;
+        static const AkUniqueID PLAY_MOVINGPLATFORMTYPEB = 904873965U;
+        static const AkUniqueID PLAY_OWBOFF = 2651187285U;
+        static const AkUniqueID PLAY_OWBON = 1297425129U;
+        static const AkUniqueID PLAY_PAUSEMENUCLOSE = 188908783U;
+        static const AkUniqueID PLAY_PAUSEMENUOPEN = 2867286185U;
+        static const AkUniqueID PLAY_PAUSEMENURETRY = 4000304925U;
+        static const AkUniqueID PLAY_PIVOTINGGATEBLOCKS = 4143950057U;
+        static const AkUniqueID PLAY_PIVOTINGGATEROTATES = 2347523137U;
+        static const AkUniqueID PLAY_PLAYERBOUNCE = 724595405U;
+        static const AkUniqueID PLAY_PLAYERBUNK = 1730740323U;
+        static const AkUniqueID PLAY_PLAYERCLIMB = 1950697752U;
+        static const AkUniqueID PLAY_PLAYERDEATH = 910581297U;
+        static const AkUniqueID PLAY_PLAYERENTERLEVEL = 343926793U;
+        static const AkUniqueID PLAY_PLAYEREXITLEVEL = 2277044357U;
+        static const AkUniqueID PLAY_PLAYERJUMP = 3817990921U;
+        static const AkUniqueID PLAY_PLAYERLAND = 658862086U;
+        static const AkUniqueID PLAY_PLAYERWALK = 2093119550U;
+        static const AkUniqueID PLAY_SWITCHOFF = 1046622137U;
+        static const AkUniqueID PLAY_SWITCHON = 823015437U;
+        static const AkUniqueID PLAY_TIMERSWITCH = 778267133U;
+        static const AkUniqueID PLAY_TIMERSWITCHENDS = 1824946087U;
+        static const AkUniqueID SETTRANSITIONSFXVOLUMETOMAX = 2216761652U;
+        static const AkUniqueID STOP_CONVEYOR = 3688398039U;
+        static const AkUniqueID STOP_ENEMYCHASING = 1671381815U;
+        static const AkUniqueID STOP_ENEMYPASSOUT = 1832694765U;
+        static const AkUniqueID STOP_ENEMYWANDER = 2590346567U;
+        static const AkUniqueID STOP_FAN = 503719263U;
+        static const AkUniqueID STOP_MOVINGPLATFORMTYPEA = 2976372756U;
+        static const AkUniqueID STOP_MOVINGPLATFORMTYPEB = 2976372759U;
+        static const AkUniqueID STOP_PLAYERCLIMB = 2353504566U;
+        static const AkUniqueID STOP_PLAYERWALK = 678353528U;
+        static const AkUniqueID STOP_TIMERSWITCH = 3883944503U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME_MUSIC
+        {
+            static const AkUniqueID GROUP = 258110631U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ENDING = 3966194980U;
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAME_MUSIC
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMBIANCEVOLUME = 365633811U;
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+        static const AkUniqueID TRANSITIONMUSICVOLUME = 116136895U;
+        static const AkUniqueID TRANSITIONSFXVOLUME = 3759402643U;
+        static const AkUniqueID TRANSITIONUIVOLUME = 3806583020U;
+        static const AkUniqueID UIVOLUME = 3415057477U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID LIGHTSOFF = 416807801U;
+        static const AkUniqueID LIGHTSOFF_MUSIC = 195207693U;
+        static const AkUniqueID LIGHTSOFF_SFX = 1713275401U;
+        static const AkUniqueID LIGHTSOFF_UI = 3405444338U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIANCES_BUS = 1869333166U;
+        static const AkUniqueID DEVICES_SFX_BUS = 1944779609U;
+        static const AkUniqueID ENEMIES_SFX_BUS = 2827189892U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID PLAYER_SFX_BUS = 3651706989U;
+        static const AkUniqueID SFX_BUS = 1502772432U;
+        static const AkUniqueID TRANSITION_MUSIC_BUS = 3386330335U;
+        static const AkUniqueID TRANSITION_SFX_BUS = 539823707U;
+        static const AkUniqueID TRANSITION_UI_BUS = 3947286728U;
+        static const AkUniqueID UI_BUS = 3600729941U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
